d71e3eadedbdfed5c0094833f48c7c02257dfce4
Maria Roig <mariaroigporta@gmail.com>
2020-02-20 19:38:10 +0100

    cambios

--- output/conversations.py
+++ output/conversations.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["emotions"]
-mycol = mydb["users"]
+mycol = mydb["conversations"]
 
-@app.route('/users/create/<name>')
-def new_users(name):
-    mydict = {"name":name}
+@app.route('/users/create/<name>/<conversation>')
+def new_conversation(name,conversation):
+    mydict = {"name":name,"conversation":conversation}
     x = mycol.insert_one(mydict)
     return x
 
